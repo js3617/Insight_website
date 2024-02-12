011c99928d76d6d6049626fbdc742df961a90ede
js3617 <a332210@naver.com>
2024-02-12 19:49:22 +0900

    member 내용 추가

---
+++ src/Member/Memberdata.js
@@ -0,0 +1,164 @@
+let member = [
+    {
+        id : 0,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 1,
+        position : "부부장",
+        name : "김봉주",
+        major : "컴퓨터공학과",
+        grade : "4학년",
+        Interests : ""
+    },
+    {
+        id : 2,
+        position : "총무",
+        name : "최영미",
+        major : "컴퓨터공학과",
+        grade : "4학년",
+        Interests : ""
+    },
+    {
+        id : 3,
+        position : "부원",
+        name : "김희준",
+        major : "컴퓨터공학과",
+        grade : "1학년(석사)",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 4,
+        position : "부원",
+        name : "류진우",
+        major : "컴퓨터공학과",
+        grade : "1학년(석사)",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 5,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 6,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 7,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 8,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 9,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 10,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 11,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 12,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 13,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 14,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 15,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 16,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 17,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 18,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+    {
+        id : 19,
+        position : "부장",
+        name : "신승우",
+        major : "컴퓨터공학과",
+        grade : "3학년",
+        Interests : "게임 기획 및 개발"
+    },
+]
+
+export default memberdata;
