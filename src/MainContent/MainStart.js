18f46c57c9a5fff24d0e6efff261ca2dd66c46c3
js3617 <a332210@naver.com>
2024-02-13 17:21:25 +0900

    첫 화면 1차 완성

--- src/MainContent/MainStart.js
+++ src/MainContent/MainStart.js
@@ -1,18 +1,19 @@
 import './Mainstyle.css';
 import Main from '../img/메인 이미지.png';
-import deu from '../img/동의대 로고.png'
+// import deu from '../img/동의대 로고.png'
 
 
 function MainStart() {
     return(
-        <>
-        <div>
-            <img src={Main} alt='Main' className='Mainimg'/>
+        <div className='maincotent'>
+            <div>
+                <img src={Main} alt='Main' className='Mainimg'/>
+            </div>
+            <div className='maintext'>
+                <p className='IT'>IT의 미래를 여는 창의적 개발의 시작, 당신의 통찰력이 필요합니다.</p>
+                <b className='Need'>Need Your <b className='insighttext'>Insight</b></b>
+            </div>
         </div>
-        <div>
-            <img src={deu} alt='deu'/>
-        </div>
-        </>
     )
 }
 
